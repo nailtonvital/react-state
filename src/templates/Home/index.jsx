587f2d401f61a751ba5fca64e7d5534aef80de66
Nailton Vital <nailtonvital35@gmail.com>
2022-07-21 22:31:23 -0300

    first commit

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+import './styles.css';
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utilis/loadPosts';
+
+class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts()
+
+    this.setState({ posts: postsAndPhotos });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <Posts posts={posts}/>
+      </section>
+    );
+  }
+}
+export default Home;
