3c7c4ff3d3691d633b3ec79b0aae3564402d3030
Nailton Vital <nailtonvital35@gmail.com>
2022-08-04 17:10:13 -0300

    home tests

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -2,6 +2,7 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { render, screen, waitForElementToBeRemoved } from "@testing-library/react"
 import { Home } from '.';
+import userEvent from '@testing-library/user-event';
 
 const handlers = [
   rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx)=>{
@@ -67,8 +68,53 @@ describe('<Home />', () => {
     render(<Home />)
     const noMorePosts = screen.getByText('Not Found')
 
+    expect.assertions(3)
+
     await waitForElementToBeRemoved(noMorePosts)
-    screen.debug()
+
+    const search = screen.getByPlaceholderText(/Search something/i)
+    expect(search).toBeInTheDocument()
+
+    const images = screen.getAllByRole('img', { name: /title/i })
+    expect(images).toHaveLength(3)
+
+    const button = screen.getByRole('button', { name: /Load More/i })
+    expect(button).toBeInTheDocument()
     
+  })
+
+  it('should search for posts ', async () => {
+    render(<Home />)
+    const noMorePosts = screen.getByText('Not Found')
+
+    // expect.assertions(3)
+
+    await waitForElementToBeRemoved(noMorePosts)
+
+    const search = screen.getByPlaceholderText(/Search something/i)
+     
+    expect(screen.getByRole('heading',{ name: 'title1' })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: 'title3' })).toBeInTheDocument()
+
+    expect(screen.queryByRole('heading', { name: 'title4' })).not.toBeInTheDocument()
+
+    // 
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search Term: title1' })).toBeInTheDocument();
+    
+    userEvent.clear(search)
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: 'title3' })).toBeInTheDocument()
+
+    userEvent.type(search, 'grfg')
+    expect(screen.getByText('Not Found')).toBeInTheDocument()
   })
 })
